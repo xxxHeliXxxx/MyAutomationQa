ce7908eb20e962558e78def45f67741bb393d5be
xxxHeliXxxx <atilinin92@gmail.com>
2022-01-03 18:27:48 +0200

    HomeWork20_CheckCartSelenide

--- src/test/java/HomeWork20_CheckingAddToCartSelenideTest.java
+++ src/test/java/HomeWork20_CheckingAddToCartSelenideTest.java
@@ -1,7 +1,7 @@
 import cartPage.CartPageLogic;
 import categoryPage.CategoryPageElements;
-import mainPage.MainPageLogic;
 import com.codeborne.selenide.Configuration;
+import mainPage.MainPageLogic;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -12,6 +12,7 @@ import static com.codeborne.selenide.Selenide.open;
 public class HomeWork20_CheckingAddToCartSelenideTest {
     private final CartPageLogic cartPageLogic = new CartPageLogic();
     private final SearchPageLogic searchPageLogic = new SearchPageLogic();
+    private final MainPageLogic mainPageLogic = new MainPageLogic();
 
     @BeforeMethod
     public void before() {
@@ -24,21 +25,19 @@ public class HomeWork20_CheckingAddToCartSelenideTest {
         new MainPageLogic().clickOnCategory(new MainPageLogic().categorySideBar)
                 .clickOnCategory(new CategoryPageElements().laptopCategory);
 
-        int indexOfProduct = 0;
-        String titleOfProductOnSearchPage = searchPageLogic.titleOfProduct(searchPageLogic.titles, indexOfProduct);
-        String priceOfProductOnSearchPage = searchPageLogic.priceOfProduct(searchPageLogic.prices, indexOfProduct);
-
+        int indexProduct = 0;
+        String titleOfProductOnSearchPage = searchPageLogic.titleOfProduct(searchPageLogic.titles, indexProduct);
+        String priceOfProductOnSearchPage = searchPageLogic.priceOfProduct(searchPageLogic.prices, indexProduct);
         searchPageLogic.addProductToCart(searchPageLogic.addToCartBtn);
-
         String cartCounter = searchPageLogic.getCartCounterText(searchPageLogic.counterOfCartProducts);
         Assert.assertEquals(cartCounter, "1");
 
         searchPageLogic.clickOnCartButton(searchPageLogic.cartBtn);
 
-        String titleOfProductOnCart = cartPageLogic.titleProduct(cartPageLogic.titleProductOnCartPage);
-        String priceOfProductOnCart = cartPageLogic.priceProduct(cartPageLogic.priceProductOnCartPage);
+        String titleOfProductCart = cartPageLogic.titleProduct(cartPageLogic.titleProductOnCartPage);
+        String priceOfProductCart = cartPageLogic.priceProduct(cartPageLogic.priceProductOnCartPage);
 
-        Assert.assertEquals(titleOfProductOnCart, titleOfProductOnSearchPage);
-        Assert.assertEquals(priceOfProductOnCart, priceOfProductOnSearchPage);
+        Assert.assertEquals(titleOfProductCart, titleOfProductOnSearchPage);
+        Assert.assertEquals(priceOfProductCart, priceOfProductOnSearchPage);
     }
 }
